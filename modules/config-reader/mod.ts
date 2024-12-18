5eb0cf9be320169bedb00a1a05f349e5fd8784b4
tony <ayakovlenko@users.noreply.github.com>
2024-12-18 10:37:34 +0800

    feat(config-reader)!: drop jsonc support

--- modules/config-reader/mod.ts
+++ modules/config-reader/mod.ts
@@ -1,26 +1,13 @@
-import { parse as parseJsonc } from "@std/jsonc/parse";
-import { parse as parseYaml } from "@std/yaml/parse";
+import { parse } from "@std/yaml/parse";
 import { Project, validate } from "../config/types.ts";
 
 function loadConfig(filename: string): Project {
   const content = Deno.readTextFileSync(filename);
 
-  let parseFunc: (s: string) => unknown;
-  if (filename.endsWith(".json") || filename.endsWith(".jsonc")) {
-    parseFunc = parseJsonc;
-  } else {
-    parseFunc = parseYaml;
-  }
+  const parsed = parse(content);
 
-  return __loadConfigFromString(content, parseFunc);
-}
-
-function __loadConfigFromString(
-  content: string,
-  parseFunc: (s: string) => unknown,
-): Project {
-  const parsed = parseFunc(content);
   const project = validate(parsed);
+
   return project;
 }
 
